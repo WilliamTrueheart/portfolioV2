7cfa47755e5ed62506c1324426496ce64d0d3927
Will Newton <91620451+WilliamTrueheart@users.noreply.github.com>
2022-05-11 21:24:11 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -52,16 +52,13 @@ function returnToTileBoard() {
     document.getElementById("display_Education").style.display= "none";
     document.getElementById("display_Contact").style.display= "none";
     document.getElementById("mainWrapper").style.display= "flex";
-    document.getElementById('showButton1').innerHTML = 'display results'
     document.getElementById("portfolio_display_1").style.display= "none";
     document.getElementById("lC_Link").style.display= "none";
     document.getElementById("foxmithLink").style.display= "none";
     document.getElementById("homebase_Link").style.display= "none";   
     document.getElementById("invite_Link").style.display= "none";   
-    document.getElementById("portfolio_Link").style.display= "none";        
-    certContainer.style.backgroundImage = "none";
-    certContainer.innerText = 'click name to retrieve certificate' ;
-
+    document.getElementById("portfolio_Link").style.display= "none";     
+    document.getElementById("rwb_Link").style.display= "none";      
 }
 
 
@@ -70,61 +67,67 @@ const portfolio_Selection = (num) => {
 
     switch (num) {
     case 1:
-    document.getElementById("portfolio_Display_Title1").innerHTML= "'FizzBuzz'<br>Eloquent Javascript";
-    document.getElementById("portfolio_Desc1").innerHTML= 'Write a program to print all the numbers from 1 to 100. <br><br> For numbers divisible by 3, print "Fizz", and for numbers divisible by 5 (and not 3), print "Buzz".<br><br> Print "FizzBuzz" for numbers that are divisible by both 3 and 5.';
-    document.getElementById("showButton1").style.display= "flex";
+    document.getElementById("portfolio_Display_Title1").innerHTML= "'Hosted on AWS Amplify";
     document.getElementById("portfolio_Desc1").style.top = "50px";
-    document.getElementById("portfolio_Desc1").style.display= "flex";
-    document.getElementById("portfolio_Link").style.display= "none";     
+    document.getElementById("portfolio_Desc1").style.display= "none";
+    document.getElementById("portfolio_Link").style.display= "none";   
+    document.getElementById("rwb_Link").style.display= "flex";   
+    document.getElementById("foxmithLink").style.display= "none";
+    document.getElementById("homebase_Link").style.display= "none";     
+    document.getElementById("invite_Link").style.display= "none";    
+    document.getElementById("lC_Link").style.display= "none";
         break;
     case 2:
     document.getElementById("portfolio_Display_Title1").innerHTML= "Below is the link to my <br> first front end project <br> on AWS Amplify";
     document.getElementById("portfolio_Desc1").style.display= "none";
     document.getElementById("portfolio_Desc1").style.top = "100px";
-    document.getElementById("showButton1").style.display= "none";
     document.getElementById("foxmithLink").style.display= "flex";
     document.getElementById("homebase_Link").style.display= "none";     
     document.getElementById("invite_Link").style.display= "none";    
     document.getElementById("lC_Link").style.display= "none";
-    document.getElementById("portfolio_Link").style.display= "none";     
+    document.getElementById("portfolio_Link").style.display= "none";  
+    document.getElementById("rwb_Link").style.display= "none";     
         break;
     case 3:
     document.getElementById("portfolio_Display_Title1").innerHTML= "Hosted on AWS Amplify";
     document.getElementById("portfolio_Desc1").style.display= "none";
     document.getElementById("portfolio_Desc1").style.top = "100px";
-    document.getElementById("showButton1").style.display= "none";
     document.getElementById("lC_Link").style.display= "none";
     document.getElementById("homebase_Link").style.display= "none";     
     document.getElementById("invite_Link").style.display= "none";  
-    document.getElementById("portfolio_Link").style.display= "flex";        
+    document.getElementById("portfolio_Link").style.display= "flex";  
+    document.getElementById("rwb_Link").style.display= "none";    
+    document.getElementById("foxmithLink").style.display= "none";    
         break;
     case 4:
     document.getElementById("portfolio_Display_Title1").innerHTML= "First attempt at cloning a website (front end)";
     document.getElementById("portfolio_Desc1").style.display= "none";
-    document.getElementById("showButton1").style.display= "none";
     document.getElementById("homebase_Link").style.display= "none";     
     document.getElementById("invite_Link").style.display= "none";  
     document.getElementById("lC_Link").style.display= "flex";
     document.getElementById("portfolio_Link").style.display= "none";     
+    document.getElementById("rwb_Link").style.display= "none";  
+    document.getElementById("foxmithLink").style.display= "none";
         break;    
     case 5:
     document.getElementById("portfolio_Display_Title1").innerHTML= "Animation practice on personal music site";
     document.getElementById("portfolio_Desc1").style.display= "none";
-    document.getElementById("showButton1").style.display= "none";
     document.getElementById("homebase_Link").style.display= "flex";    
     document.getElementById("invite_Link").style.display= "none";  
     document.getElementById("lC_Link").style.display= "none";
     document.getElementById("portfolio_Link").style.display= "none";     
+    document.getElementById("rwb_Link").style.display= "none";  
+    document.getElementById("foxmithLink").style.display= "none";
         break;
     case 6:
     document.getElementById("portfolio_Display_Title1").innerHTML= "Practice with forms and animation";
     document.getElementById("portfolio_Desc1").style.display= "none";
-    document.getElementById("showButton1").style.display= "none";
     document.getElementById("homebase_Link").style.display= "none";  
     document.getElementById("invite_Link").style.display= "flex";  
     document.getElementById("lC_Link").style.display= "none";
     document.getElementById("portfolio_Link").style.display= "none";     
-
+    document.getElementById("rwb_Link").style.display= "none";  
+    document.getElementById("foxmithLink").style.display= "none";
     }
 }
 
@@ -134,53 +137,11 @@ const returnFromPortfolioItem = () => {
     document.getElementById('showButton1').innerHTML = 'display results'
     document.getElementById("foxmithLink").style.display= "none";
     document.getElementById("lC_Link").style.display= "none";
-    document.getElementById("showButton1").style.display= "none";
     document.getElementById("homebase_Link").style.display= "none";    
     document.getElementById("invite_Link").style.display= "none";  
     document.getElementById("portfolio_Link").style.display= "none";     
-
+    document.getElementById("rwb_Link").style.display= "none";  
    
 }
 
-const fizzBuzz = () => {
-    let fizzBuzz = [];
 
-    for (i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            fizzBuzz.push('FizzBuzz')
-        }
-        else if (i % 3 == 0) {
-            fizzBuzz.push('Fizz')
-        }
-        else if (i % 5 == 0) {
-            fizzBuzz.push('Buzz')
-        }
-        else {
-            fizzBuzz.push(i)
-        }
-    }
-
-    document.getElementById('showButton1').innerHTML = fizzBuzz.join(', ')
-}
-
-
-const selectCert = (num) => {
-    const certContainer = document.getElementById('certContainer');
-
-    if (num == 1) {
-        certContainer.innerText = '' ;
-        certContainer.style.backgroundImage = "url('htmlCert.png')";
-    }
-    if (num == 2) {
-        certContainer.innerText = '' ;
-        certContainer.style.backgroundImage = "url('jsCert.png')";
-    }
-    if (num == 3) {
-        certContainer.innerText = '' ;
-        certContainer.style.backgroundImage = "url('sqlCert.png')";
-    }
-    if (num == 4) {
-        certContainer.innerText = 'upon request' ;
-        certContainer.style.backgroundImage = "none";
-        }
-}
